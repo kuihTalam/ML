c68f26fa650bfa9980e2064d5f15c121c39e2b6d
kuihTalam <talamkuih@gmail.com>
2017-03-05 11:25:16 +0800

    Add files via upload

---
+++ svr.py
@@ -0,0 +1,54 @@
+# SVR
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv(r'C:\Users\songheng\Desktop\Zach\ML\PolynomialRegression\Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values # '1:2' change it to matrice number
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set 
+# no need to split dataset because we don't have enough dataset to do that
+'''from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)'''
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+# Fitting the SVR to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+# Predicting a new result
+# The X need to re-scale back by using sc_X.transform
+# '.transform' has to be in array-form thus np.array is used.
+# 'inverse.transform' is used to transform back
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+
+
+# Visualising the SVR results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (SVR Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
